d2175e8985cabdd85ab1164f6bb1a7f2dea99244
Mario <lartu@seznam.cz>
2023-03-13 17:30:55 +0100

    done without optionals

--- resources/js/front/src/App.jsx
+++ resources/js/front/src/App.jsx
@@ -3,17 +3,15 @@ import { useState } from "react";
 import "./App.scss";
 import LeftMenu from "./components/LeftMenu";
 import MainContent from "./components/MainContent";
-
 function App() {
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <LeftMenu />
-
-        <MainContent />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <LeftMenu />
+                <MainContent />
+            </div>
+        </div>
+    );
 }
 
 export default App;
